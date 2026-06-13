1ef42ee11d9fe52f2fb0050db15d99b231791bc1
asolab-company <asolab.company@gmail.com>
2026-06-13 17:51:20 +0200

    Update AppLinks.swift

--- RemoveBG/Config/AppLinks.swift
+++ RemoveBG/Config/AppLinks.swift
@@ -3,7 +3,7 @@ import Foundation
 enum AppLinks {
     static let privacyPolicy = URL(string: "https://sites.google.com/view/remove-bg/privacy-policy")!
     static let termsOfUse = URL(string: "https://sites.google.com/view/remove-bg/terms-of-use")!
-    static let appStore = URL(string: "https://apps.apple.com/app/id6767313502")!
+    static let appStore = URL(string: "https://apps.apple.com/app/id6779994364")!
 
     static let shareSubject = "Remove BG"
     static let shareMessage = "Remove backgrounds instantly with AI. Try Remove BG:"
